4e14340cb7735428ce033d1a8bc3d599ea8bdd45
marc <cei4bio@gmail.com>
2022-08-20 18:38:17 +0600

    Скрыты данные пользователя.

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -20,7 +20,7 @@ router.register(r'users', UserViewSet, basename='user')
 
 
 urlpatterns = [
-    path('v1/auth/signup/', signup, name='singup'),
+    path('v1/auth/signup/', signup, name='signup'),
     path('v1/auth/token/', get_token, name='get_token'),
     path('v1/', include(router.urls)),
 ]
